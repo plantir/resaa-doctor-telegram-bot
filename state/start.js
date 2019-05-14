5e5bde8df258320d0f924216af06c5d6f836b777
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-05-14 10:18:33 +0430

    add chatid to start state

--- state/start.js
+++ state/start.js
@@ -1,28 +1,31 @@
-const User = require('../Model/User')
-const bot = require('../bot')
-const _enum = require('../config/enum')
-const start_video = 'BAADBAAD0wUAAtRquVDOyHp6nv1negI'
+const User = require('../Model/User');
+const bot = require('../bot');
+const _enum = require('../config/enum');
+const start_video = 'BAADBAAD0wUAAtRquVDOyHp6nv1negI';
 
 bot.onText(/شروع|بازگشت به خانه|start/, async msg => {
-  let user = new User(msg.chat.id)
-  user.reset_state_history()
-  user.state = _enum.state.start
-  let phone = await user.phone
-  let message = `به رسا خوش آمدید\nجهت آشنایی بیشتر با رسا این ویدیو رو تماشا نمایید`
+  let user = new User(msg.chat.id);
+  user.reset_state_history();
+  user.state = _enum.state.start;
+  let phone = await user.phone;
+  let message = `به رسا خوش آمدید\nجهت آشنایی بیشتر با رسا این ویدیو رو تماشا نمایید`;
   let options = {
     reply_markup: {
       keyboard: [],
       resize_keyboard: true
     }
-  }
+  };
   if (phone) {
-    options.reply_markup.keyboard.push([{
-      text: 'شارژ اعتبار رسا'
-    }])
+    options.reply_markup.keyboard.push([
+      {
+        text: 'شارژ اعتبار رسا'
+      }
+    ]);
   }
   options.reply_markup.keyboard.push(
     ...[
-      [{
+      [
+        {
           text: 'سوال پزشکی دارم'
         },
         {
@@ -30,10 +33,16 @@ bot.onText(/شروع|بازگشت به خانه|start/, async msg => {
         }
       ]
     ]
-  )
-  options.reply_markup.keyboard.push([{
-    text: 'تماس با پشتیبانی'
-  }])
-  options.caption = message
-  await bot.sendMessage(msg.chat.id, 'hello')
-})
+  );
+  options.reply_markup.keyboard.push([
+    {
+      text: 'تماس با پشتیبانی'
+    }
+  ]);
+  options.caption = message;
+  await bot.sendMessage(msg.chat.id, 'hello');
+});
+
+bot.onText(/chatid/g, msg => {
+  bot.sendMessage(msg.chat.id, msg.chat.id);
+});
