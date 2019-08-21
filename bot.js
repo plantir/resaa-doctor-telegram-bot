5a6b4163770cb9b9eb8f49ca2453a5bd8fce6dfb
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-08-21 21:17:10 +0430

    change port

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-var TelegramBot = require('node-telegram-bot-api')
-const token = '388067630:AAHwHSYHfX7zdFrCKPIC1oi14wvZcxWqc_c'
-const User = require('./Model/User')
+require('dotenv').config();
+var TelegramBot = require('node-telegram-bot-api');
+const token = '388067630:AAHwHSYHfX7zdFrCKPIC1oi14wvZcxWqc_c';
+const User = require('./Model/User');
 class Bot extends TelegramBot {
   async sendMessage(chatId, text, body = {}, whith_history = true) {
     if (whith_history) {
-      let user = new User(chatId)
+      let user = new User(chatId);
       await user.push_history({
         text,
         body
-      })
+      });
     }
-    super.sendMessage(chatId, text, body)
+    super.sendMessage(chatId, text, body);
   }
 }
 var bot = new Bot(token, {
-  polling: true
-})
-module.exports = bot
+  polling: process.env.MODE == 'polling' ? true : false
+});
+module.exports = bot;
